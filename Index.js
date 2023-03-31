9bd98330ed2d87b712c90a4b4972dc9c8ef295e6
ChamathDMahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2023-03-31 21:23:33 +0530

    Delete unwanted

--- Index.js
+++
@@ -1,5 +0,0 @@
-// Hide the splash screen after 3 seconds
-  setTimeout(function() {
-    var splashScreen = document.getElementById("splash-screen");
-    splashScreen.style.display = "none";
-  }, 12000);
